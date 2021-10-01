dd67103e94e077c09967f00843f56d59b2641362
akshatabharatecognitiveclouds.com <akshata.bharate@cognitiveclouds.com>
2021-10-01 16:36:49 +0530

    added commands and directions

--- app/controllers/api/robots_controller.rb
+++ app/controllers/api/robots_controller.rb
@@ -1,8 +1,8 @@
 class Api::RobotsController < ApplicationController
     require './lib/robo.rb'
     def orders
-        input=params[:commands]
-        @result=Robot.execute(input)
+        input = params[:commands]
+        @result = Robot.execute(input)
         render json: @result , status: :ok
     end
 end
